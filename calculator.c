f7f2a223643e8fb1cd99ef7807381fbd13b79202
sakett26 <67407102+sakett26@users.noreply.github.com>
2020-06-26 16:26:20 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d,e,f,g;
+    printf("the numbers are\n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("sum of a and b is %d\n",c);
+    d=a-b;
+    printf("difference between a and b is %d\n",d);
+    e=a*b;
+    printf("product of a and b is %d\n ",e);
+    f=a/b;
+    printf("division of a and b is %d\n",f);
+    g=a%b;
+    printf("modulas of a and b is %d\n",g);
+    return 0;
+}
+
