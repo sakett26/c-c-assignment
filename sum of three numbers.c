6b6dbe3c3ca92205a316914867a957d2f5f5beb8
sakett26 <67407102+sakett26@users.noreply.github.com>
2020-06-26 15:35:16 +0530

    Add files via upload

---
+++ sum of three numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d;
+    printf("enter the  number\n");
+    scanf("%d%d%d",&a,&b,&c);
+    d=a+b+c;
+    printf("sum of numbers are %d",d);
+    return 0;
+    }
